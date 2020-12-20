89a264ad25e3f0ebb7ab12be2184f889cc6a50cd
Paulo Ferraz <paulosgf.pf@gmail.com>
2020-12-20 19:29:52 -0300

    ENHANCE: Add alarm support

--- app/src/main/java/com/paulosgf/samadhi/MainActivity.java
+++ app/src/main/java/com/paulosgf/samadhi/MainActivity.java
@@ -10,17 +10,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.Build;
+
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnStore, btnGetall;
+    private Button btnStore, btnGetall, btnStartAlarm, btnCancelAlarm;
     private EditText etmsg;
     private DatabaseHelper databaseHelper;
+
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnStartAlarm = findViewById(R.id.btnStartAlarm);
+        btnCancelAlarm = findViewById(R.id.btnCancelAlarm);
+
+        // database logic
         TextView textView = findViewById(R.id.textview);
         Typeface typeface = ResourcesCompat.getFont(
                 this, R.font.kaushanscript_regular);
@@ -48,5 +60,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // alarm service logic
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent alarmIntent = new Intent(this, MyBroadCastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+        btnStartAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAlarm();
+            }
+        });
+
+        btnCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+    }
+
+    // alarm methods
+    private void startAlarm() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        } else {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        }
+    }
+
+    private void cancelAlarm() {
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(getApplicationContext(), "Alarm Cancelled", Toast.LENGTH_LONG).show();
     }
 }
